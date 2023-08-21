5937529b71f8f9d02453651ed30481e518e5ae21
Carlos Pereira Atencio <carlosperate@embeddedlog.com>
2023-08-22 00:06:20 +0100

    src: Extra LED functions into module, use basic microdot.

---
+++ src/append_libs_path.py
@@ -0,0 +1,5 @@
+import sys
+import os
+
+libs_path = os.path.join(os.path.abspath(os.path.dirname(__file__)), 'libs/')
+sys.path.append(libs_path)
---
+++ src/libs/README.md
@@ -0,0 +1,6 @@
+# Vendored MicroPython libs
+
+## microdot
+
+v1.3.3:
+- https://github.com/miguelgrinberg/microdot/releases/tag/v1.3.3
