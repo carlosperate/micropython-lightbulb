ea52146f22e66b5ac0b4136c04e494a0066cf825
Carlos Pereira Atencio <carlosperate@embeddedlog.com>
2023-08-25 01:02:04 +0100

    src: Add index.html & REST PUT bulb colour request.

--- src/leds.py
+++ src/leds.py
@@ -1,12 +1,33 @@
 import time
 import machine
 
+
 pin_white_cold = machine.PWM(machine.Pin(5))
 pin_white_warm = machine.PWM(machine.Pin(13))
 pin_r = machine.PWM(machine.Pin(4))
 pin_g = machine.PWM(machine.Pin(12))
 pin_b = machine.PWM(machine.Pin(14))
 all_pwm_pins = (pin_r, pin_g, pin_b, pin_white_warm, pin_white_cold)
+
+COLOUR_RANGE = (0, 255)
+PWM_RANGE = (0, 768)
+
+
+def map(value, from_range, to_range):
+    return int(
+        to_range[0] + \
+            (((value - from_range[0]) / (from_range[1] - from_range[0])) * \
+                (to_range[1] - to_range[0]))
+    )
+
+
+def set_all(red, green, blue, white_cold, white_warm):
+    print((red, green, blue, white_cold, white_warm))
+    pin_r.duty(map(red, COLOUR_RANGE, PWM_RANGE))
+    pin_g.duty(map(green, COLOUR_RANGE, PWM_RANGE))
+    pin_b.duty(map(blue, COLOUR_RANGE, PWM_RANGE))
+    pin_white_cold.duty(map(white_cold, COLOUR_RANGE, PWM_RANGE))
+    pin_white_warm.duty(map(white_warm, COLOUR_RANGE, PWM_RANGE))
 
 
 def off():
@@ -20,10 +41,10 @@ def flash_pwm():
     for pwm_pin in all_pwm_pins:
         for i in range(1024):
             pwm_pin.duty(i)
-            time.sleep_ms(1)
+            time.sleep(0.001)
         for i in reversed(range(1024)):
             pwm_pin.duty(i)
-            time.sleep_ms(1)
+            time.sleep(0.001)
 
 
 def flash():
--- src/main.py
+++ src/main.py
@@ -1,5 +1,5 @@
-import append_libs_path
-from microdot import Microdot
+import libs_path
+from microdot import Microdot, send_file
 import time
 import leds
 
@@ -9,12 +9,26 @@ app = Microdot()
 
 @app.route('/')
 def index(request):
-    return 'Hello, world!'
+    return send_file('static/index.html')
+
+
+@app.put('/colour')
+def set_light(request):
+    lights = request.json
+    leds.set_all(
+        red=lights["r"],
+        green=lights["g"],
+        blue=lights["b"],
+        white_cold=lights["wc"],
+        white_warm=lights["ww"],
+    )
+    return { "success": True }
 
 
 def main():
     app.run(host='0.0.0.0', port=5000, debug=True, ssl=None)
 
+    # This will never run
     while True:
         leds.flash_pwm()
         time.sleep(1)
